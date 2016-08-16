100d10add511ae6adfadfd75c34ff4ed22027e6a
Ella Rose <python_pride@protonmail.com>
2016-08-16 09:59:08 -0700

    added rough draft

--- crypto/booleanfunctions.py
+++ crypto/booleanfunctions.py
@@ -4,13 +4,14 @@ import pprint
 from cryptanalysis import summarize_sbox
 from utilities import integer_to_bytes
 
-def choice(b, c, d):
-    return d ^ (b & (c ^ d))
+def choice(a, b, c):
+    return c ^ (a & (b ^ c))
     
-def majority(b, c, d):
-    return (b & c) | (b & d) | (c & d)     
+def majority(a, b, c):
+    return (a & b) | (a & c) | (b & c)     
     
-def test_choice():
+def test_choice(): 
+    
     sbox = {(_input, choice(*_input)) for _input in (tuple(bytearray(integer_to_bytes(integer, 3))) for integer in xrange(2 ** 24))}
     print "Done "
     with open("choice_sbox.txt", 'w') as _file:
@@ -22,5 +23,5 @@ def test_choice():
     summarize_sbox(sbox)
     
 if __name__ == "__main__":
-    test_choice()
+    #test_choice()    
     
--- crypto/coreprf2.py
+++ crypto/coreprf2.py
@@ -59,18 +59,78 @@ def round_function(state, state2):
     # end decorrelation layer
     
     # non linear layer + mix the rows
-   # t y t2 y2
-    t ^= choice(y, t2, y2)
-    t = micksRow(t)
     
+    ## t y t2 y2
+    #t ^= choice(y, t2, y2)
+    #t = micksRow(t)
+    #
+    #y ^= choice(t2, y2, t)
+    #y = micksRow(y)
+    #
+    #t2 ^= choice(y2, t, y)
+    #t2 = micksRow(t2)
+    #
+    #y2 ^= choice(t, y, t2)
+    #y2 = micksRow(y2)               
+    
+    
+    t ^= micksRow(choice(y, t2, y2))
+    y ^= micksRow(choice(t2, y2, t))
+    t2 ^= micksRow(choice(y2, t, y))
+    y2 ^= micksRow(choice(t, y, t2))
+    
+    #t ^= micksRow(y ^ t2 ^ y2)
+    #y ^= micksRow(t2 ^ y2 ^ t)
+    #t2 ^= micksRow(y2 ^ t ^ y)
+    #y2 ^= micksRow(t ^ y ^ t2)
+    
+    state =  (y << 32) | t        
+    state2 = (y2 << 32)| t2     
+    
+    return state, state2
+    
+def prf(state, state2):    
+    # top half   
+    # the strange ordering applies shuffle_bytes before the bit permutation
+ 
+    x = (((state2 >> 24) & 255) << 24) | (((state >> 40) & 255) << 16) | (((state >> 32) & 255) << 8) | ((state2 >> 56) & 255)
+    y = (((state2 >> 32) & 255) << 24) | (((state >> 48) & 255) << 16) | (((state2 >> 8) & 255) << 8) | (state & 255)    
+    
+    t = (y ^ (y >> 7)) & 0x00AA00AA;  y = y ^ t ^ (t << 7);    
+    t = (x ^ (x >>14)) & 0x0000CCCC;  x = x ^ t ^ (t <<14); 
+    t = (y ^ (y >>14)) & 0x0000CCCC;  y = y ^ t ^ (t <<14); 
+    
+    t = (x & 0xF0F0F0F0) | ((y >> 4) & 0x0F0F0F0F); 
+    y = ((x << 4) & 0xF0F0F0F0) | (y & 0x0F0F0F0F); 
+            
+    # bottom half
+    x =  (((state2 >> 40) & 255) << 24) | (((state >> 24) & 255) << 16) | (((state2 >> 48) & 255) << 8) | (state2 & 255)
+    y2 = (((state >> 8) & 255) <<24)  | (((state2 >> 16) & 255) << 16) | (((state >> 16) & 255) << 8) | ((state >> 56) & 255)      
+    
+    t2 = (y2 ^ (y2 >> 7)) & 0x00AA00AA;  y2 = y2 ^ t2 ^ (t2 << 7); 
+    
+    t2 = (x ^ (x >>14)) & 0x0000CCCC;  x = x ^ t2 ^ (t2 <<14); 
+    t2 = (y2 ^ (y2 >>14)) & 0x0000CCCC;  y2 = y2 ^ t2 ^ (t2 <<14); 
+    
+    t2 = (x & 0xF0F0F0F0) | ((y2 >> 4) & 0x0F0F0F0F); 
+    y2 = ((x << 4) & 0xF0F0F0F0) | (y2 & 0x0F0F0F0F);                    
+    # end decorrelation layer
+    
+    # non linear layer + mix the rows
+    ## t y t2 y2
+    t ^= choice(y, t2, y2) 
+  #  t = micksRow(t ^ y ^ t2 ^ y2)    
     y ^= choice(t2, y2, t)
-    y = micksRow(y)
-    
+  #  y = micksRow(t ^ y ^ t2 ^ y2)    
     t2 ^= choice(y2, t, y)
-    t2 = micksRow(t2)
+  #  t2 = micksRow(t ^ y ^ t2 ^ y2)   
+    y2 ^= choice(t, y, t2) 
+  #  y2 = micksRow(t ^  y ^ t2 ^ y2)
     
-    y2 ^= choice(t, y, t2)
-    y2 = micksRow(y2)               
+    t ^=  micksRow(t ^ y ^ y2 ^ t2)
+    y ^=  micksRow(t ^ y ^ y2 ^ t2)
+    t2 ^= micksRow(t ^ y ^ y2 ^ t2)
+    y2 ^= micksRow(t ^ y ^ y2 ^ t2)
     
     state =  (y << 32) | t        
     state2 = (y2 << 32)| t2     
@@ -120,7 +180,7 @@ def print_active_sbox_info():
             
 def test_round_function():
     from utilities import print_state_4x4, integer_to_bytes
-    state, state2 = 0, 1
+    state, state2 = 1, 0
     
     state = polarize(state)
     state, state2 = round_function(state, state2)

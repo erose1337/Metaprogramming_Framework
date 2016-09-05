2ff5ea69b2f6783d3935fc21a4160acd9e7649e0
Ella Rose <python_pride@protonmail.com>
2016-09-04 17:37:40 -0700

    progress on c version

--- crypto/booleanstreampcipher.c
+++ crypto/booleanstreampcipher.c
@@ -1,18 +1,58 @@
-from utilities import xor_subroutine, rotate_left, bytes_to_words, integer_to_bytes
+#include <stdlib.h>
+#include <stdio.h>
 
-#DEFINE WORDSIZE unsigned long long
-#DEFINE ROUNDS 1
+#define WORDSIZE unsigned long long
+#define ROUNDS 1
 
-WORDSIZE choice(WORDSIZE a, WORDSIZE b, WORDSIZE c):
-    return c ^ (a & (b ^ c))
-        
-WORDSIZE nonlinear_mixing(WORDSIZE b, WORDSIZE c, WORDSIZE d): 
-    return rotate_left(choice(b, c, d), 1, bit_width=64)        
-    
-void _stream_cipher(WORDSIZE* nonce, WORDSIZE* key, WORDSIZE* output, WORDSIZE* constants)
+#ifndef memcpy_s
+void memcpy_s(WORDSIZE* s1, WORDSIZE* s2, size_t n)
+{
+    unsigned int index;
+    for (index = 0; index < n; index++)
+    {
+        s1[index] = s2[index];
+    }
+}
+#endif
+
+WORDSIZE rotate_left(WORDSIZE word64, int amount)
+{    
+    return ((word64 << amount) | (word64 >> (64 - amount)));
+}
+
+void xor_subroutine(WORDSIZE* data1, WORDSIZE* data2, WORDSIZE size)
+{
+    WORDSIZE index = 0;
+    for (index = 0; index < size; index++)
+    {
+        data1[index] ^= data2[index];
+    }
+}
+
+WORDSIZE choice(WORDSIZE a, WORDSIZE b, WORDSIZE c)
+{
+    return c ^ (a & (b ^ c));
+}
+       
+WORDSIZE nonlinear_mixing(WORDSIZE b, WORDSIZE c, WORDSIZE d, int rotation_amount)
+{
+    return rotate_left(choice(b, c, d), rotation_amount);      
+}
+   
+void _stream_cipher(WORDSIZE* nonce, WORDSIZE* key, WORDSIZE* keystream, WORDSIZE* constants, WORDSIZE block_count)
 { 
-    k1, k2, k3, k4 = bytes_to_long_longs(key)
-    s1, s2, s3, s4 = bytes_to_long_longs(nonce)
+    WORDSIZE block = 0;
+    int round = 0;
+    
+    WORDSIZE k1 = key[0];
+    WORDSIZE k2 = key[1];
+    WORDSIZE k3 = key[2];
+    WORDSIZE k4 = key[3];
+    
+    WORDSIZE s1 = nonce[0];
+    WORDSIZE s2 = nonce[1];
+    WORDSIZE s3 = nonce[2];
+    WORDSIZE s4 = nonce[3];       
     
     for (block = 0; block < block_count; block++)
     {
@@ -23,60 +63,97 @@ void _stream_cipher(WORDSIZE* nonce, WORDSIZE* key, WORDSIZE* output, WORDSIZE* 
             s3 ^= k3 ^ constants[round ^ 7];
             s4 ^= k4 ^ constants[round ^ 13];
             
-            s1 ^= nonlinear_mixing(s2, s3, s4);
-            s2 ^= nonlinear_mixing(s3, s4, s1);
-            s3 ^= nonlinear_mixing(s4, s1, s2);
-            s4 ^= nonlinear_mixing(s1, s2, s3);
+            s1 ^= nonlinear_mixing(s2, s3, s4, 3);
+            s2 ^= nonlinear_mixing(s3, s4, s1, 5);
+            s3 ^= nonlinear_mixing(s4, s1, s2, 7);
+            s4 ^= nonlinear_mixing(s1, s2, s3, 13);
             
             k1 ^= constants[round ^ 17];
             k2 ^= constants[round ^ 19];
             k3 ^= constants[round ^ 23];
             k4 ^= constants[round ^ 29];
             
-            k1 ^= nonlinear_mixing(k2, k3, k4);
-            k2 ^= nonlinear_mixing(k3, k4, k1);
-            k3 ^= nonlinear_mixing(k4, k1, k2);
-            k4 ^= nonlinear_mixing(k1, k2, k3);
+            k1 ^= nonlinear_mixing(k2, k3, k4, 17);
+            k2 ^= nonlinear_mixing(k3, k4, k1, 19);
+            k3 ^= nonlinear_mixing(k4, k1, k2, 23);
+            k4 ^= nonlinear_mixing(k1, k2, k3, 29);
         }   
         s1 ^= k1;
         s2 ^= k2;
         s3 ^= k3;
         s4 ^= k4;
-        memcpy_s(output + 0, &s1, sizeof(s1));
-        memcpy_s(output + 1, &s2, sizeof(s2));
-        memcpy_s(output + 2, &s3, sizeof(s3));
-        memcpy_s(output + 3, &s4, sizeof(s4));                     
+        printf("pre memcpy\n");
+        memcpy_s(keystream + 0, &s1, sizeof(s1));
+        memcpy_s(keystream + 1, &s2, sizeof(s2));
+        memcpy_s(keystream + 2, &s3, sizeof(s3));
+        memcpy_s(keystream + 3, &s4, sizeof(s4));     
+        printf("Post memcpy\n");
     }
+}
     
-def crypt_stream(plaintext, key, nonce, rounds=16):        
-    data = bytearray(plaintext)
-    data_size = len(data)
-    output = bytearray()  
-    
-    _stream_cipher(bytearray(nonce), bytearray(key), output, data_size, rounds)    
-    xor_subroutine(data, output)    
-    return bytes(data)
+void crypt_stream(WORDSIZE* plaintext, WORDSIZE* key, WORDSIZE* nonce, WORDSIZE* constants, WORDSIZE blocks)
+{
+    printf("Allocating memory\n");
+    WORDSIZE* keystream = (WORDSIZE*)malloc(blocks * 32 * sizeof(WORDSIZE)); 
+    if (keystream == NULL)
+    {
+        printf("Failed to allocate memory\n");
+    }
+    printf("Entering stream cipher\n");
+    _stream_cipher(nonce, key, keystream, constants, blocks);  
+    printf("combining with keystream\n");
+    xor_subroutine(plaintext, keystream, blocks);
+    printf("after xor\n");
+    free(keystream);
+    printf("Freed keystream\n");
+}    
        
-def test_crypt_stream():
-    message = bytearray(32)    
-    key = bytearray(32)
-    nonce = bytearray(32)    
-    nonce[-1] = 1
-    message[1] = 2
-    _message = message[:]
+void test_crypt_stream()
+{
+    WORDSIZE message[4], key[4], nonce[4], _message[4], constants[256];
+    int index = 0;
+    for (index = 0; index < 4; index++)
+    {
+        message[index] = 0;
+        _message[index] = 0;
+        key[index] = 0;
+        nonce[index] = 0;
+    }
+    for (index = 0; index < 256; index++)
+    {
+        constants[index] = index;
+    }
+    //nonce[-1] = 1
+    //message[1] = 2
+    printf("Encrypting\n");    
+    crypt_stream(message, key, nonce, constants, 1);
+    printf("Decrypting\n");
+
+    for (index = 0; index < 4; index++)
+    {
+        printf("%I64u\n", message[index]);        
+    }    
+    printf("\n");
     
-    crypt_stream(message, key, nonce)
-        
-    #print "Ciphertext: {}\n{}".format(len(message), [byte for byte in message])
-    
-    crypt_stream(message, key, nonce)
-    assert message == _message, (message, _message)
-    
-def test_crypt_stream_metrics():
-    from metrics import test_stream_cipher
-    test_stream_cipher(crypt_stream, bytearray(32), bytearray(32))
-        
-if __name__ == "__main__":
-    test_crypt_stream()
-    test_crypt_stream_metrics()        
+    crypt_stream(message, key, nonce, constants, 1);
+    printf("....Done\n");
+    for (index = 0; index < 4; index++)
+    {
+        if (message[index] != _message[index])
+        {
+            printf("Failed\n");
+        }
+        else
+        {
+            printf("%I64u", message[index]);
+        }
+    }    
+    printf("\n");
+}
+
+int main()
+{
+    test_crypt_stream();
+    return 0;
+}
     

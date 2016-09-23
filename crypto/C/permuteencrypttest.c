6b68176f79981d117c4a9d0415c4e28b66534132
Ella Rose <python_pride@protonmail.com>
2016-09-22 22:29:43 -0700

    work backup

--- crypto/C/permuteencrypttest.c
+++ crypto/C/permuteencrypttest.c
@@ -1,7 +1,8 @@
 #include "performancetesting.c" // for test_encrypt_performance_4x32
 
 #define WORDSIZE32 unsigned long
-#define ITERATIONS 2
+#define ITERATIONS 10
+#define KEY_COUNT 3
 
 #define rotate_left(x, amount)(x << amount | (x >> (32 - amount)))
     
@@ -27,8 +28,12 @@
         permutation(a, b, c, d);}\
 })    
     
-#define add_key(a, b, c, d, k0, k1, k2, k3)\
-    ({a ^= k1; b ^= k1; c ^= k2; d ^= k3;})                       
+#define add_key(a, b, c, d, keys, key_number)\
+({  a ^= keys[(4 * key_number) + 0];\
+    b ^= keys[(4 * key_number) + 1];\
+    c ^= keys[(4 * key_number) + 2];\
+    d ^= keys[(4 * key_number) + 3];\
+})                       
     
 #define store(data, a, b, c, d)\
     ({data[0] = a; data[1] = b; data[2] = c; data[3] = d;})   
@@ -36,25 +41,31 @@
 #define load_abcd(data)\
     ({a = data[0]; b = data[1]; c = data[2]; d=data[3];})
     
-#define load_key(key, k0, k1, k2, k3)\
-    ({k0 = key[0]; k1 = key[1]; k2 = key[2]; k3 = key[3];})
+#define choice_swap(key, a, b)\
+({  _t = a;\
+    a = choice(key, a, b);\
+    b = choice(key, b, _t);\
+})
     
-#define load_round_key(round_key)\
-    ({rk0 = round_key[0]; rk1 = round_key[1]; rk2 = round_key[2]; rk3 = round_key[3];})
-        
+#define nonlinear_keyed_bit_permutation(a, b, c, d, round_key, key)\
+({  choice_swap(key[(4 * key_number) + 0], a, b);\
+    choice_swap(key[(4 * key_number) + 1], c, d);\
+    choice_swap(key[(4 * key_number) + 2], a, c);\
+    choice_swap(key[(4 * key_number) + 3], b, d);\
+})    
+
 void key_schedule(WORDSIZE32* key, WORDSIZE32* round_keys)
 {    
     int key_number;           
-    WORDSIZE32 a, b, c, d;
-    WORDSIZE32 k0, k1, k2, k3; 
-    load_key(key, k0, k1, k2, k3);  
-    a = k0; b = k1; c = k2; d = k3;
-    for (key_number = 0; key_number < 3; key_number++)
+    WORDSIZE32 a, b, c, d;    
+    load_abcd(key);
+    
+    for (key_number = 0; key_number < KEY_COUNT; key_number++)
     {        
         permutation(a, b, c, d); 
-        add_key(a, b, c, d, k0, k1, k2, k3);
+        add_key(a, b, c, d, key, 0);
         store(round_keys, a, b, c, d); 
-        add_key(a, b, c, d, k0, k1, k2, k3); // remove key
+        add_key(a, b, c, d, key, 0); // remove key
         
         round_keys += 4;
     }
@@ -62,24 +73,26 @@ void key_schedule(WORDSIZE32* key, WORDSIZE32* round_keys)
         
 void encrypt(WORDSIZE32* data, WORDSIZE32* key)
 {       
-    WORDSIZE32 round_keys[12], a, b, c, d;
+    WORDSIZE32 round_keys[4 * KEY_COUNT], a, b, c, d, _t;
     load_abcd(data);
-    key_schedule(key, round_keys);
-        
+    key_schedule(key, round_keys);        
+    
     int key_number;
-    for (key_number = 0; key_number < 3; key_number++)
+    for (key_number = 0; key_number < KEY_COUNT - 1; key_number++)
     {        
-        add_key(a, b, c, d, round_keys[(4 * key_number) + 0], round_keys[(4 * key_number) + 1], round_keys[(4 * key_number) + 2], round_keys[(4 * key_number) + 3]);   
-        iterate(permutation, a, b, c, d, ITERATIONS);
+        add_key(a, b, c, d, round_keys, key_number);
+        nonlinear_keyed_bit_permutation(a, b, c, d, round_keys, key_number);
+        iterate(permutation, a, b, c, d, ITERATIONS);        
     }
-    add_key(a, b, c, d, round_keys[0], round_keys[1], round_keys[2], round_keys[3]);
+    add_key(a, b, c, d, round_keys, (KEY_COUNT - 1));
     store(data, a, b, c, d);
 }
- 
-
+     
 int main()
 {
     WORDSIZE32 data[4], key[4];
+    key[0] = 1;
     encrypt(data, key);
     test_encrypt_performance_4x32(encrypt, 5000000);
+    return 0;
 }

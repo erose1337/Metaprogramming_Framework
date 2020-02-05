613adc54d5f11abf7c57f673b2eb035f4ddb5634
Ella Rose <python_pride@protonmail.com>
2020-02-05 15:09:37 -0500

    refactoring shell to use Scrollable_Text_Window

--- pride/gui/programs/shell.py
+++ pride/gui/programs/shell.py
@@ -2,141 +2,121 @@ import subprocess
 import tempfile
 import getpass
 import shlex
+from math import ceil
 
 import pride.gui.widgets.form
+import pride.gui.widgets.formext
 
 field_info = pride.gui.widgets.form.field_info
 
-class Shell(pride.gui.widgets.form.Scrollable_Window):
+class Shell(pride.gui.widgets.formext.Scrollable_Text_Window):
 
-    defaults = {"text_field" : '', "input_field" : '', "stdin" : None,
-                "line_count" : 24, "offset" : 0, "process" : None}
-    mutable_defaults = {"input_history" : list, "lines" : list,
-                        "vertical_slider_entry_kwargs" :\
-                            lambda: {"orientation" : "stacked",
-                                     "include_minmax_buttons" : False,
-                                     "hide_text" : True}}
-    hotkeys = {('\n', None) : "handle_return"}
-    autoreferences = ("_form", )
-
-    def __init__(self, **kwargs):
-        super(Shell, self).__init__(**kwargs)
-        self.create_subcomponents()
-
-    def create_subcomponents(self):
-        window = self.main_window
-        fields = [[field_info("text_field", auto_create_id=False,
-                              editable=False,
-                              entry_kwargs={"center_text" : False,
-                                            "hoverable" : False,
-                                            "font" : "Hack-Regular"})],
-                  [field_info("input_field", orientation="side by side",
-                              field_type=pride.gui.widgets.form.Text_Field,
-                              parent_shell=self,
-                              display_name=self._get_user_string(),
-                              entry_kwargs={"center_text" : False,
-                                            "font" : "Hack-Regular"})]
-                 ]
-        form = window.create(pride.gui.widgets.form.Form, fields=fields,
-                             target_object=self)
-        form.rows[1].h_range = (24, 24)
-        self._form = form
+    defaults = {"stdin" : None, "process" : None}
 
     def _get_user_string(self):
         return "{}@{}:{}".format(getpass.getuser(), os.uname()[1], os.getcwd())
 
+    def create_subcomponents(self):
+        super(Shell, self).create_subcomponents()
+        self.write_to(self._get_user_string() + ' ')
+
+    def handle_backspace(self):
+        if self.lines[-1] != self._get_user_string() + ' ':
+            super(Shell, self).handle_backspace()
+
     def handle_return(self):
-        input_field = self.input_field; lines = self.lines
-        if not input_field:
+        super(Shell, self).handle_return()
+        command = self.lines[-2][len(self._get_user_string()) + 1:] # -2 because handle_return creates a new line; +1 because of space
+        if not command.strip():
+            #print("No command -2: {} -1: {}".format(self.lines[-2], self.lines[-1]))
+            self.write_to(self._get_user_string() + ' ')
             return
-        if self.stdin is not None:
-            new_line = input_field
+
+        if self.process is not None:
+            self.poll_process() # will clear .process and .std* if necessary
+
+        if self.process is not None:
+            print("Input: {}".format(command))
+            stdin = self.process.stdin
+            stdin.write(command + '\n')
+            stdin.flush()
         else:
-            new_line = self._get_user_string() + ' ' + input_field
-        lines.append(new_line)
+            self.run_command(command)
+        self.read_stdout()
 
-        self.input_history.append(input_field)
-        self.input_field = ''
+        self.lines.append(self._get_user_string() + ' ')
+        self.synchronize_scroll_bars()
 
-        if self.stdin is not None:
-            process = self.process
-            _stdin = process.stdin
-            _stdin.write(input_field)
-            _stdin.flush()
-        else:
-            pipe = subprocess.PIPE
-            stdout = pipe#tempfile.TemporaryFile()#None#pipe
-            stderr = subprocess.STDOUT
-            stdin = tempfile.TemporaryFile()
-            args = shlex.split(input_field)
-            try:
-                process = subprocess.Popen(args, stdout=stdout, stderr=stderr,
-                                           stdin=stdin)
-            except OSError as error:
-                if error.errno != 2:
-                    raise
-                return
-            self.process = process
+        if self.process is not None:
+            self.poll_process()
 
-        stdout = process.stdout
+    def run_command(self, command):
+        assert self.process is None
+        args = shlex.split(command)
+        stdout = stderr = open("_prideshell.temp", "wb")
+        self.stdout = open("_prideshell.temp", 'r')
+        stdin = subprocess.PIPE
+        print("Command: {} ({})".format(command, args))
+        try:
+            process = subprocess.Popen(args, stdout=stdout, stderr=stderr,
+                                       stdin=stdin)
+        except OSError as error:
+            if error.errno not in (2, 13):
+                raise
+            if error.errno == 13 and os.path.exists(command):
+                if os.path.isdir(command):
+                    self.write_to("Shell: {}: Is a directory".format(command))
+                else:
+                    self.write_to("Shell: {}: Permission denied".format(command))
+            print("Ignoring {} '{}'".format(error, command))
+            self.lines.append(self._get_user_string() + ' ')
+            self.y_scroll_value += 1
+            self.synchronize_scroll_bars()
+            return
+        import time
+        time.sleep(.01)
+        self.process = process
+        #self.sdl_window.schedule_postdraw_operation(self.check_output, self)
+
+    def read_stdout(self):
+        stdout = self.stdout
         if stdout is not None:
+            stdout.flush()
             output = stdout.read()
+            #print("Read {} bytes".format(len(output)))
             if output:
                 output = output.split('\n')
-                lines.extend(output)
+                self.write_to(output.pop(0))
+                if any(output):
+                    self.lines.extend(output)
                 if not self.vertical_slider.hidden:
-                    self.offset += len(output)
-                    self.synchronize_scroll_bars()
+                    self.y_scroll_value += len(output)
+#                    self.synchronize_scroll_bars()
+#                else:
+#                    self.update_text_field()
 
-        code = process.wait()
+    def poll_process(self):
+        code = self.process.poll()
         # > A None value indicates that the process hasn’t terminated yet.
         #      -> not None indicates the process has terminated
-        if code is not None and input_field != "python":
+        if code is not None:
             print("Process terminated with code {}".format(code))
-            self.stdin = None
-            self.process = None
+            if self.stdin is not None:
+                self.stdin.close()
+            if self.stdout is not None:
+                self.stdout.close()
+            self.stdin = self.stdout = self.process = None
         else:
             print("Process did NOT terminate")
-
-        assert self.vertical_slider is not None # relies on below to call `update_text_field`
-        self.synchronize_scroll_bars()
-
-    def update_text_field(self):
-        self.text_field = '\n'.join(self.lines[self.offset:self.offset + self.line_count])
-        #print("Wrote {} lines:".format(len(self.text_field.split('\n'))))
-        #print(self.text_field)
-        self._form.synchronize_fields()
-
-    def handle_area_change(self, old_area):
-        super(Shell, self).handle_area_change(old_area)
-        font_size = self.sdl_window.renderer.font_manager.wrapped_object.size
-        self.line_count = int(float(self._form.rows[0].h) / (font_size + 3))
-        self.synchronize_scroll_bars()
-
-        #if not self.lines:
-        #    self.lines = ['\n'] * self.line_count
-
-    def handle_y_scroll(self, old_value, new_value):
-        super(Shell, self).handle_y_scroll(old_value, new_value)
-        self.offset = new_value
-        self.update_text_field()
-
-    def synchronize_scroll_bars(self):
-        slider = self.vertical_slider
-        if slider is not None:
-            slider.maximum = max(0, len(self.lines) - self.line_count)
-            slider.update_position_from_value()
-            slider.pack()
-            self.update_text_field()
-            #entry = slider.entry
-            #entry.max_button.entry.texture_invalid = True
-            #entry.min_button.entry.texture_invalid = True
+        return code
 
     def delete(self):
         if self.process is not None:
-            self.process.terminate()
+            if self.process.returncode is None:
+                self.process.terminate()
             self.process = None
         super(Shell, self).delete()
+
 
 def test_Shell():
     import pride.gui

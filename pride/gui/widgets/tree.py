f73686f5733d94e854ec1fe9c905caa32b80f240
Ella Rose <python_pride@protonmail.com>
2020-09-23 16:32:04 -0400

    update to use layouts

--- pride/gui/widgets/tree.py
+++ pride/gui/widgets/tree.py
@@ -2,14 +2,17 @@ import os
 import time
 
 import pride.gui.gui
-import pride.gui.widgets.form
+import pride.gui.form
 from pride.functions.utilities import slide
+from pride.gui.form import field_info, row_info, layout
 
 
-class Node_Viewer(pride.gui.widgets.form.Form):
+class Node_Viewer(pride.gui.form.Form):
 
     pass
 
+
+# change current_node theme profile to alert if it does not exist
 
 class Tree_Viewer(pride.gui.gui.Application):
 
@@ -24,19 +27,21 @@ class Tree_Viewer(pride.gui.gui.Application):
 
     def create_subcomponents(self):
         window = self.application_window
-        fields=[
-                [("handle_back", {"button_text" : '<',
-                                  "scale_to_text" : True}),
-                 ("handle_up", {"button_text" : "/\\", "scale_to_text" : True}),
-                 ("current_node", {"display_name" : self.node_label}),
-                 ("view_node", {"button_text" : "view",
-                                "scale_to_text" : True}),
-                 ("delete", {"button_text" : 'x', "scale_to_text" : True})
-                ]
-               ]
-        self.top = window.create(pride.gui.widgets.form.Form, location="top",
+        _layout = layout(
+                    row_info(0,
+                        field_info("handle_back", button_text='<',
+                                   scale_to_text=True),
+                        field_info("handle_up", button_text="/\\",
+                                   scale_to_text=True),
+                        field_info("current_node",
+                                   display_name=self.node_label),
+                        field_info("view_node", button_text="view",
+                                   scale_to_text=True),
+                        field_info("delete", button_text='x',
+                                   scale_to_text=True)))
+        self.top = window.create(pride.gui.form.Form, location="top",
                                  target_object=self, h_range=(0, .1),
-                                 fields=fields)
+                                 layout=_layout)
         self.bottom = window.create("pride.gui.gui.Container", location="top")
 
     def view_node(self, identifier=None): # "view" button does not use identifier and uses self.current_node
@@ -47,11 +52,11 @@ class Tree_Viewer(pride.gui.gui.Application):
         children = self.lookup(identifier)
         if children is None:
             return
-        fields = self.children_to_fields(children)
+        _layout = layout(*self.children_to_fields(children))
         if self.viewer is not None:
             self.viewer.delete()
         self.viewer = self.bottom.create(Node_Viewer, target_object=self,
-                                         fields=fields, row_h_range=(0, .15),
+                                         layout=_layout, row_h_range=(0, .15),
                                          max_rows=6)
         if self.history: # need to check for contents before testing [-1] in case history is empty
             if self.history[-1] != identifier:
@@ -76,13 +81,15 @@ class Tree_Viewer(pride.gui.gui.Application):
         raise NotImplementedError()
 
     def children_to_fields(self, children):
-        return [[self.new_entry(child) for child in chunk] for
-                 chunk in slide(children, self.max_count)]
+        return [row_info(i,
+                        *[self.new_entry(child) for child in chunk]) for
+                 i, chunk in enumerate(slide(children, self.max_count))]
 
     @staticmethod
     def new_entry(child):
-        return ("view_node", {"button_text" : getattr(child, "text", str(child)),
-                              "args" : (child, ), "scale_to_text" : False})
+        return field_info("view_node",
+                          button_text=getattr(child, "text", str(child)),
+                          args=(child, ), scale_to_text=False)
 
     def lookup(self, identifier):
         """ lookup should return a sequence of items that are children of the current node """

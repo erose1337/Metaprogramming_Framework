280824a9a1f8ffb6cb1edb27a60ff286417901d4
Ella Rose <python_pride@protonmail.com>
2020-11-16 17:13:50 -0500

    fixed registration

--- pride/programs/register.py
+++ pride/programs/register.py
@@ -2,6 +2,7 @@ import sys
 
 import pride.components.base
 import pride.components.user
+import pride.functions.contextmanagers
 
 class Service_Registration(pride.components.base.Base):
     """ Registers a new 'user' (not a `User` object) with an Authenticated_Service.
@@ -11,19 +12,42 @@ class Service_Registration(pride.components.base.Base):
         This is written as a Base object instead of as a function so that the built-in argument parsing can be used.
         """
     defaults = {"name" : "registration", "username" : '', "password" : '',
-                "authentication_client_name" : "pride.components.interpreter.Shell"}
+                "target_service" : "/Program/Interpreter",
+                "auto_exit" : True}
     parser_args = ("username", "password")
+    verbosity = {"registration_success" : 0, "registration_failure" : 0}
 
     def __init__(self, **kwargs):
         super(Service_Registration, self).__init__(**kwargs)
-        username = self.username or pride.objects["/User"].username
-        password = self.password or pride.objects["/User"].password
-        client = pride.objects["/Program"].create(self.authentication_client_name,
-                                                  auto_login=False,
-                                                  username=username, password=password,
-                                                  _register_callback=sys.exit)#lambda status: sys.exit())
-        client.register()
 
+        if "/User" not in pride.objects:
+            msg = "/User not found; probable cause: Program not run using pride"
+            msg += "\nRun using python -m pride.main yourmodule.py ..."
+            raise SystemExit(msg)
+        username = self.username
+        user = pride.objects["/User"]
+        if not username:
+            username = user.username
+
+        service_name = self.target_service
+        password = self.password
+        if not password:
+            password = user.generate_strong_password(service_name, username)
+
+        service = pride.objects[service_name]
+        with pride.functions.contextmanagers.backup(service, "allow_registration"):
+            service.allow_registration = True
+            success = service.register(username, password)
+        if success:
+            message = "Successfully registered '{}' with {}"
+            self.alert(message.format(username, service_name),
+                       level=self.verbosity["registration_success"])
+        else:
+            message = "Failed to register '{}' with {}"
+            self.alert(message.format(username, service_name),
+                       level=self.verbosity["registration_failure"])
+        if self.auto_exit:
+            raise SystemExit()
 
 class User_Registration(pride.components.user.User):
 
